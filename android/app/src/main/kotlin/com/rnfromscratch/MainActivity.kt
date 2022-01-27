5a4773e3e6cea477c37e212a04bbcffc136523d9
Raul Max <raulmax319@gmail.com>
2022-01-27 08:23:43 -0300

    Minor fixes

--- android/app/src/main/kotlin/com/rnfromscratch/MainActivity.kt
+++ android/app/src/main/kotlin/com/rnfromscratch/MainActivity.kt
@@ -6,11 +6,4 @@ import android.content.res.Configuration
 
 class MainActivity: ReactActivity() {
   override fun getMainComponentName(): String? = "rnfromscratch"
-
-  override fun onConfigurationChanged(newConfig: Configuration) {
-    super.onConfigurationChanged(newConfig)
-    val intent = Intent("onConfigurationChanged")
-    intent.putExtra("newConfig", newConfig)
-    this.sendBroadcast(intent)
-  }
 }
