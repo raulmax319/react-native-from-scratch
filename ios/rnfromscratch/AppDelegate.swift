3216c4d908523baf1a9abd65329456f130e59762
Raul Max <raulmax319@gmail.com>
2022-02-02 20:34:55 -0300

    style: fix AppDelegate's indentation

--- ios/rnfromscratch/AppDelegate.swift
+++ ios/rnfromscratch/AppDelegate.swift
@@ -10,30 +10,30 @@ import React
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    let bridge: RCTBridge = RCTBridge.init(delegate: self, launchOptions: nil)
+    let rootView: RCTRootView = RCTRootView(bridge: bridge, moduleName: "rnfromscratch", initialProperties: nil)
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        let bridge: RCTBridge = RCTBridge.init(delegate: self, launchOptions: nil)
-        let rootView: RCTRootView = RCTRootView(bridge: bridge, moduleName: "rnfromscratch", initialProperties: nil)
-        
-        self.window = UIWindow.init()
-        
-        let rootViewController: UIViewController = UIViewController.init()
-        rootViewController.view = rootView
-        self.window!.rootViewController = rootViewController
-        self.window?.makeKeyAndVisible()
-        
-        return true
-    }
+    self.window = UIWindow.init()
+    
+    let rootViewController: UIViewController = UIViewController.init()
+    rootViewController.view = rootView
+    self.window!.rootViewController = rootViewController
+    self.window?.makeKeyAndVisible()
+    
+    return true
+  }
 }
 
 extension AppDelegate: RCTBridgeDelegate {
-    func sourceURL(for bridge: RCTBridge!) -> URL! {
-        #if DEBUG
-        return RCTBundleURLProvider.jsBundleURL(forBundleRoot: "index", packagerHost: "localhost", enableDev: true, enableMinification: false)
-        #else
-        return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
-        #endif
-    }
+  func sourceURL(for bridge: RCTBridge!) -> URL! {
+#if DEBUG
+    return RCTBundleURLProvider.jsBundleURL(forBundleRoot: "index", packagerHost: "localhost", enableDev: true, enableMinification: false)
+#else
+    return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+#endif
+  }
 }
 
